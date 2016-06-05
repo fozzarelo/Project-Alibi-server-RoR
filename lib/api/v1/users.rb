7089bc244160e568fc374935713e9e5f74011201
Dan Pinto <fozzarelo@gmail.com>
2016-06-04 19:52:12 -0700

    added picker for contacts, also modifications to signup so that it can serve as a contact registrator.

--- lib/api/v1/users.rb
+++ lib/api/v1/users.rb
@@ -17,9 +17,8 @@ module API
         post :signin do
           u = User.find_by_email(params[:email])
           contacts  = u.contactings.map{|c| [c.nickname, c.contact.email]}
-          puts Hash[contacts]
-          user = {username: u.username, email: u.email, contacts: Hash[contacts]}
-          return user if u && u.authenticate(params[:password])
+          user = {error: u.errors, username: u.username, email: u.email, contacts: Hash[contacts]}
+          return user if u && u.authenticate(params[:password]) && u.has_account
         end
 
         desc 'Sign up a user'
@@ -30,10 +29,31 @@ module API
         end
 
         post :signup do
-          user = User.create(username: params[:username], password: params[:password], email: params[:email])
-          error!({error: user.errors.full_messages}, 400) if !user.persisted?
+          u = User.create(username: params[:username], password: params[:password], email: params[:email], has_account: true)
+          error!({error: u.errors.full_messages}, 400) if !u.persisted?
+          user = {error: u.errors.full_messages, username: u.username, email: u.email, contacts: {}}
+          puts user
           return user
         end
+
+        desc 'Add a contact a user'
+        params do
+          requires :contactNickname, type: String
+          requires :contactEmail, type: String
+        end
+
+        #TODO arregla esto para que grabe el contacting. tienes que encontrar los user id con el mail. el nick ya viene
+        # Despues, assegurate de mandar la nueva lista de contactos, esa va al async storage de contacts
+        # y quita ese byebug!
+        post :addContact do
+          u = User.create(email: params[:contactEmail], has_account: false, username: '__blank__', password: '__blank__')
+          cont = Contacting.create(nickname: ,user_id:, contact_id:)
+          byebug
+          # user = {error: u.errors.full_messages, username: u.username, email: u.email, contacts: {}}
+          puts user
+          return u
+        end
+
       end
     end
   end
